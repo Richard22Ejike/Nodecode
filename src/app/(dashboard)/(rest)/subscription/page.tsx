a99f969b6f0b104e85e5e26617476417846d8eb6
richard <richard.ekene22@outlook.com>
2026-06-28 23:35:57 +0100

    removed test

--- src/app/(dashboard)/(rest)/subscription/page.tsx
+++ src/app/(dashboard)/(rest)/subscription/page.tsx
@@ -1,24 +1,24 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
-import React from "react";
-import { toast } from "sonner";
+// import { Button } from "@/components/ui/button";
+// import { useTRPC } from "@/trpc/client";
+// import { useMutation } from "@tanstack/react-query";
+// import React from "react";
+// import { toast } from "sonner";
 
-const Page = () => {
-  const trpc = useTRPC();
-  const testAI = useMutation(
-    trpc.testAi.mutationOptions({
-      onSuccess: () => {
-        toast.success("Success");
-      },
-      onError: ({ message }) => {
-        toast.error(message);
-      },
-    })
-  );
-  return <Button onClick={() => testAI.mutate()}>Test AI</Button>;
-};
+// const Page = () => {
+//   const trpc = useTRPC();
+//   const testAI = useMutation(
+//     trpc.testAi.mutationOptions({
+//       onSuccess: () => {
+//         toast.success("Success");
+//       },
+//       onError: ({ message }) => {
+//         toast.error(message);
+//       },
+//     })
+//   );
+//   return <Button onClick={() => testAI.mutate()}>Test AI</Button>;
+// };
 
-export default Page;
+// export default Page;
