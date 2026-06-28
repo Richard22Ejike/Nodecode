25c34b6e0f280bb9c7b09a22681e608d007a14f2
richard <richard.ekene22@outlook.com>
2026-06-28 23:51:09 +0100

    sub fixed

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,16 @@
 import { createTRPCRouter } from "../init";
 import { workflowsRouter } from "@/features/workflows/server/routers";
+import {
+   baseProcedure
 
+} from "@/trpc/init";
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouter,
+  testAi: baseProcedure
+    .mutation(async () => {
+      // Your AI test logic here
+      return { success: true };
+    }),
 });
 
 export type AppRouter = typeof appRouter;
