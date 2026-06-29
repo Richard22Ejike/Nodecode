c91d2a3986ac89ad8d288f74e06460945d0907da
richard <richard.ekene22@outlook.com>
2026-06-29 16:50:20 +0100

    update auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,31 +6,38 @@ import { dash } from "@better-auth/infra";
 import prisma from "@/lib/db";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3002",
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  emailAndPassword: {
+    emailAndPassword: {
     enabled: true,
     autoSignIn: true,
   },
+ 
   plugins: [
     dash(),
-    polar({
-      client: polarclient,
-      createCustomerOnSignUp: true,
-      use: [
-        checkout({
-          products: [
-            {
-             productId: "95f0d378-4a1f-48e4-baf9-8c0ab0fbd8c8",
-             slug: "base-pro"
-            },
-          ],
-          successUrl: process.env.POLAR_SUCCESS_URL,
-          authenticatedUsersOnly: true,
-        }),
-        portal(),
-      ],
-    }),
+  //   polar({
+  //     client: polarclient,
+  //     createCustomerOnSignUp: true,
+  //      trustedOrigins: [
+  //   "http://localhost:3002",
+  //   "http://localhost:3000",
+ 
+  // ],
+  //     use: [
+  //       checkout({
+  //         products: [
+  //           {
+  //            productId: "95f0d378-4a1f-48e4-baf9-8c0ab0fbd8c8",
+  //            slug: "base-pro"
+  //           },
+  //         ],
+  //         successUrl: process.env.POLAR_SUCCESS_URL,
+  //         authenticatedUsersOnly: true,
+  //       }),
+  //       portal(),
+  //     ],
+  //   }),
   ],
 });
